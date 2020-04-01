d371b50a099f7a8777aed8a03ca628ee4ae49dc1
adtapper <adamtapper55@gmail.com>
2020-04-01 13:32:18 -0400

    Update and style front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,30 @@
 import React from 'react';
 
 export const App = () => {
+  const handleChange = () => {
+
+  }
+
+  const calculateInterest = () => {
+
+  }
+
   return(
-    <div>Hello World</div>
+    <div>
+      <form>
+        <label className='label'>Loan Type</label>
+        <select onChange={handleChange}>
+            <option value="car-personal-home">Car/Personal/Home</option>
+            <option value="credit-card">Credit Card</option>
+        </select>
+        <label className='label'>Principal</label>
+        <input className = 'startDate' type="number" maxLength="7" placeholder="0"></input>
+        <label className='label'>Interest Rate</label>
+        <input className = 'endDate' type="number" maxLength="3" placeholder="0"></input>
+        <label className='label'>Loan Length</label>
+        <input className = 'endDate' type="number" maxLength="3" placeholder="0"></input>
+        <button onClick={calculateInterest}>Calculate Interest</button>
+      </form>
+    </div>
   )
 }
