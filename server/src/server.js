1a468238a53a90ed15a528f347d0b1ccc99ffad5
adtapper <adamtapper55@gmail.com>
2020-04-01 12:51:52 -0400

    Setup basic server

---
+++ server/src/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send('Hello World!'));
+app.post('/interest', function (req, res) {
+    res.send('POST request to the homepage')
+})
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
